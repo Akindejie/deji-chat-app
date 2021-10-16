040655696496a9e78cf3395ab3cb2afd6ef5d419
Akindejie <fuddisnalk@gmail.com>
2021-10-16 12:04:37 +0100

    deji-chat

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { ChatEngine } from "react-chat-engine";
+import LoginForm from "./components/LoginForm";
+import ChatFeed from "./components/ChatFeed";
+
+import "./App.css";
+
+const App = () => {
+  if (!localStorage.getItem("username")) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="3f50a06a-5e2c-42ce-a6a8-6b92c3edae1d"
+      userName={localStorage.getItem("username")}
+      userSecret={localStorage.getItem("password")}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+    />
+  );
+};
+
+export default App;
